18bde2f3edb7a9949ab1e7b863356ef527309e80
Harkály Gergő <git@harkalygergo.hu>
2026-03-09 18:02:03 +0100

    add nelmio/cors-bundle to prevent CORS error

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lelyfoto\Twig\InstanceOf\TwigInstanceOfBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
