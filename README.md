672e67be930a114ef0f25439b731c00054729e2d
Harkály Gergő <git@harkalygergo.hu>
2026-03-25 17:48:30 +0100

    remove app fixtures

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ⫹⫺ PLATFORM
-###### v2026.03.25.7
+###### v2026.03.25.8
 
 ![PLATFORM dashboard](/_docs/images/platform.png?raw=true "PLATFORM dashboard")
 
--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lelyfoto\Twig\InstanceOf\TwigInstanceOfBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
