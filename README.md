eff9bb786c88b059bef9d6807c845cba4d3353fc
Harkály Gergő <git@harkalygergo.hu>
2026-04-22 12:00:10 +0200

    composer and npm update, plus repair error: Impossible to access an attribute ("originalName") on a null

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ⫹⫺ PLATFORM
-###### v2026.04.21.5
+###### v2026.04.22.1
 
 ![PLATFORM dashboard](/_docs/images/platform.png?raw=true "PLATFORM dashboard")
 
--- src/Form/Platform/Website/WebsiteType.php
+++ src/Form/Platform/Website/WebsiteType.php
@@ -15,9 +15,17 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class WebsiteType extends AbstractType
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $currentWebsite = $options['data'] ?? null; // Get the current Website entity
@@ -30,31 +38,35 @@ class WebsiteType extends AbstractType
                 'label' => 'Domain',
             ])
             ->add('name', TextType::class, [
-                'label' => 'Name',
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('global.name'),
             ])
             ->add('title', TextType::class, [
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('global.title'),
             ])
             ->add('slogan', TextType::class, [
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('global.slogan'),
             ])
             ->add('description', TextareaType::class, [
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('data.description'),
             ])
             ->add('phone', TextType::class, [
                 'required' => false,
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('user.phone'),
             ])
             ->add('email', TextType::class, [
                 'required' => false,
@@ -67,6 +79,7 @@ class WebsiteType extends AbstractType
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'label' => $this->translator->trans('address.address'),
             ])
             ->add('facebook', TextType::class, [
                 'required' => false,
